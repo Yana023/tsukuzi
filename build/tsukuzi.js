444e6fff3a98590b7228b76cd2a9d16f27a5aa42
yana <yana.kanae@gmail.com>
2017-10-14 20:19:44 +0900

    init

---
+++ build/tsukuzi.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var w = 10 * 2; // 1charactor width
+context.font = "10pt Arial";
+context.fillText("鶏油", 0, 11);
+context.scale(w / 10, 1);
+context.fillText("衣", 0, 23); // 例：からあげとか。
+//# sourceMappingURL=tsukuzi.js.map
